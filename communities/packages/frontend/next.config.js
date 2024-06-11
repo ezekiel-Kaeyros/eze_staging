6474069b3d74367c785649af1f2491e8f353a530
ezekiel-kaeyros <ezekiel.dombissi@kaeyros-analytics.com>
2024-06-11 18:46:49 +0100

    pusj

---
+++ communities/packages/frontend/next.config.js
@@ -0,0 +1,13 @@
+const { i18n } = require('./next-i18next.config');
+const nextConfig = {
+  i18n,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
